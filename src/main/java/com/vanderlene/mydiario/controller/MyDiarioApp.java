a113799104847370cacf23f6002f33c2b3802658
vanderleneo <vanderleneo>
2025-08-13 15:48:56 +0200

    HU-06: Implementar opción para salir del programa

--- src/main/java/com/vanderlene/mydiario/controller/MyDiarioApp.java
+++ src/main/java/com/vanderlene/mydiario/controller/MyDiarioApp.java
@@ -1,34 +1,24 @@
 package com.vanderlene.mydiario.controller;
 
-import java.time.LocalDate;
-
-import com.vanderlene.mydiario.model.DiarioServicio;
-import com.vanderlene.mydiario.model.Emocion;
-import com.vanderlene.mydiario.model.Momento;
 import com.vanderlene.mydiario.view.ConsoleView;
 
 public class MyDiarioApp {
-    private static DiarioServicio diarioServicio = new DiarioServicio();
     private static ConsoleView view = new ConsoleView();
 
     public static void main(String[] args) {
+        view.mostrarMensaje("Bienvenido a Mi Diario");
 
-        
-        diarioServicio.agregarMomento(
-            new Momento("Cumpleaños", "Fui a una fiesta", Emocion.ALEGRIA, LocalDate.now())
-        );
+        String opcion = "";
+        while (!opcion.equalsIgnoreCase("salir")) {
+            opcion = view.leerTexto("Escriba 'salir' para cerrar la aplicación: ");
+        }
 
-      
-        Emocion emocion = view.leerEmocion("Ingrese la emoción para filtrar: ");
-        view.mostrarMensaje("Momentos con emoción " + emocion + ":");
-        view.mostrarMomentos(diarioServicio.filtrarPorEmocion(emocion));
-
-       
-        LocalDate fechaFiltro = view.leerFecha("Ingrese la fecha a filtrar (dd/MM/yyyy): ");
-        view.mostrarMensaje("Momentos en la fecha " + fechaFiltro + ":");
-        view.mostrarMomentos(diarioServicio.filtrarPorFecha(fechaFiltro));
+        view.mostrarMensaje("Aplicación cerrada. ¡Hasta luego!");
     }
+    
 }
+
+
 
 
 
--- src/main/java/com/vanderlene/mydiario/view/ConsoleView.java
+++ src/main/java/com/vanderlene/mydiario/view/ConsoleView.java
@@ -47,7 +47,14 @@ public class ConsoleView {
             }
         }
     }
+
+    public String leerTexto(String mensaje) {
+        System.out.print(mensaje);
+        return scanner.nextLine();
+    }
 }
+
+
 
 
 
