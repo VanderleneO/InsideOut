efa15ac4b9cc4d7a1366b182974babdf727d9f5f
vanderleneo <vanderleneo>
2025-08-14 13:34:51 +0200

    feat: refactorizacion del metodo filtrar  po emocion

--- src/main/java/com/vanderlene/mydiario/controller/MyDiarioApp.java
+++ src/main/java/com/vanderlene/mydiario/controller/MyDiarioApp.java
@@ -18,14 +18,14 @@ public class MyDiarioApp {
             new Momento("Cumpleaños", "Fui a una fiesta", Emocion.ALEGRIA, LocalDate.now())
         );
         
-        // --- Funcionalidad de filtrado por emoción ---
+     
         Emocion emocion = view.leerEmocion("Ingrese la emoción para filtrar:");
         view.mostrarMensaje("Momentos con emoción " + emocion + ":");
         view.mostrarMomentos(diarioServicio.filtrarPorEmocion(emocion));
         
         System.out.println("--------------------");
 
-        // --- Funcionalidad de filtrado por fecha ---
+     
         LocalDate fechaFiltro = view.leerFecha("Ingrese la fecha a filtrar (dd/MM/yyyy):");
         view.mostrarMensaje("Momentos en la fecha " + fechaFiltro + ":");
         view.mostrarMomentos(diarioServicio.filtrarPorFecha(fechaFiltro));
