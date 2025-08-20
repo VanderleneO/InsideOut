c335c893c98862de2a9100e4496522fdae19366f
vanderleneo <vanderleneo>
2025-08-20 14:30:12 +0200

    feat:refactorizar mi codigo

--- src/main/java/com/vanderlene/mydiario/model/DiarioServicio.java
+++ src/main/java/com/vanderlene/mydiario/model/DiarioServicio.java
@@ -17,9 +17,8 @@ public class DiarioServicio {
     public List<Momento> obtenerTodosLosMomentos() {
         return new ArrayList<>(momentos);
     }
-      public boolean eliminarMomentoPorId(int id) { 
+
+    public boolean eliminarMomentoPorId(int id) {
         return momentos.removeIf(m -> m.getId() == id);
     }
 }
-
-
